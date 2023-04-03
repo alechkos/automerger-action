c157e709a58eea71a076be30795cd760534c8ae5
Richard Scott <github@pointb.co.uk>
2023-04-03 21:44:15 +0100

    Added custom commit message variable.

--- src/index.js
+++ src/index.js
@@ -9,49 +9,49 @@ const repo = github.context.repo
 
 function slackSuccessMessage(source, target, status) {
   return {
-      color: "#27ae60",
-      icon: ":white_check_mark:",
-      message: `${source} was successfully merged into ${target}.`,
-      description: `*${target}* can be pushed to production!`
+    color: "#27ae60",
+    icon: ":white_check_mark:",
+    message: `${source} was successfully merged into ${target}.`,
+    description: `*${target}* can be pushed to production!`
   }
 }
 
 function slackErrorMessage(source, target, status) {
   return {
-      color: "#C0392A",
-      icon: ":red_circle:",
-      message: `*${source}* has confilct with *${target}*.`,
-      description: ":face_with_head_bandage: Fix me please :pray:"
+    color: "#C0392A",
+    icon: ":red_circle:",
+    message: `*${source}* has confilct with *${target}*.`,
+    description: ":face_with_head_bandage: Fix me please :pray:"
   }
 }
 
 async function slackMessage(source, target, status) {
   if (core.getInput('webhook_url')) {
     let payload = status == 'success' ?
-                  slackSuccessMessage(source, target, status) :
-                  slackErrorMessage(source, target, status)
+      slackSuccessMessage(source, target, status) :
+      slackErrorMessage(source, target, status)
 
     slack.send({
       icon_emoji: payload.icon,
       username: payload.message,
       attachments: [
-          {
-              author_name: github.context.payload.repository.full_name,
-              author_link: `https://github.com/${github.context.payload.repository.full_name}/`,
-              title: payload.message,
-              text: payload.description,
-              color: payload.color,
-              fields: [
-                  { title: 'Job Status', value: status, short: false },
-              ],
-          },
+        {
+          author_name: github.context.payload.repository.full_name,
+          author_link: `https://github.com/${github.context.payload.repository.full_name}/`,
+          title: payload.message,
+          text: payload.description,
+          color: payload.color,
+          fields: [
+            { title: 'Job Status', value: status, short: false },
+          ],
+        },
       ],
     });
   }
 }
 
-async function merge(source, target) {
-  core.info(`merge branch:${source} to: ${target}`)
+async function merge(source, target, message) {
+  core.info(message)
 
   const response = await octokit.repos.merge({
     owner: repo.owner,
@@ -65,10 +65,11 @@ async function merge(source, target) {
 async function run() {
   const source = core.getInput('source')
   const target = core.getInput('target')
-  core.info(`merge ${source} into ${target}`)
+  const message = core.getInput('message')
+  core.info(message)
 
   try {
-    await merge(source, target)
+    await merge(source, target, message)
     await slackMessage(source, target, 'success')
   } catch (error) {
     await slackMessage(source, target, 'failure')
